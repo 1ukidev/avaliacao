d9d2a69612726e4ccd013cd63fe96536c5a06b04
EmanuelFerreira <48385634+Bebel132@users.noreply.github.com>
2022-06-01 19:53:57 -0300

    adicionando classe de controle para o fazerRegistros

---
+++ POO/Avaliação/src/view/FazerRegistrosController.java
@@ -0,0 +1,38 @@
+package view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.image.ImageView;
+
+public class FazerRegistrosController {
+
+    @FXML
+    private Hyperlink ajuda;
+
+    @FXML
+    private Button btnEnviar;
+
+    @FXML
+    private ComboBox<?> diaEntrega;
+
+    @FXML
+    private ComboBox<?> equipamento;
+
+    @FXML
+    private Hyperlink fazerRegistro;
+
+    @FXML
+    private ComboBox<?> horarioEntrega;
+
+    @FXML
+    private ImageView iconConfig;
+
+    @FXML
+    private ComboBox<?> nomeProfessor;
+
+    @FXML
+    private Hyperlink verRegistros;
+
+}
