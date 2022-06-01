e4eef42431521f70c5fb059f0252c9d30d2897d1
EmanuelFerreira <48385634+Bebel132@users.noreply.github.com>
2022-06-01 19:52:49 -0300

    adicionando classe de controle para o menuInicial

---
+++ POO/Avaliação/src/view/MenuInicialController.java
@@ -0,0 +1,21 @@
+package view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.image.ImageView;
+
+public class MenuInicialController {
+
+    @FXML
+    private Hyperlink ajuda;
+
+    @FXML
+    private Hyperlink fazerRegistro;
+
+    @FXML
+    private ImageView iconConfig;
+
+    @FXML
+    private Hyperlink verRegistros;
+
+}
