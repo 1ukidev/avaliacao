dbe11ee3af898cd985618790db9f858d1b264c46
EmanuelFerreira <48385634+Bebel132@users.noreply.github.com>
2022-06-05 10:09:11 -0300

    concertando essa pasta

---
+++ PW/control/atualizarDados.js
@@ -0,0 +1,22 @@
+$("#fazendoRegistros").click((e) => { 
+    e.preventDefault();
+    let formData = new FormData();
+    formData.append("nomeProfessor", $("#nomeProfessor").val());
+    formData.append("dia", $("#dia").val());
+    formData.append("horario", $("#horario").val());
+    formData.append("equipamento", $("#equipamento").val());
+    for (var value of formData.values()) {
+        console.log(value); 
+     }
+    $.ajax({
+        url: "../model/dados.php",
+        type: "POST",
+        data: formData,
+        processData: false,
+        contentType: false,
+        success: (e) => {
+            //alertify.success('registro feito com sucesso');
+            $(".container").html(e);
+        }
+    })
+});
