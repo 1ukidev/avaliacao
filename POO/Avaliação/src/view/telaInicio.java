e53d03c81f5648c77d40e80ac6eb3663457bfc7a
Josiane <106617646+Josi987@users.noreply.github.com>
2022-06-09 08:46:59 -0300

    telaInicio.java
    ids para redirecionamento de tela.

--- POO/Avaliação/src/view/telaInicio.java
+++ POO/Avaliação/src/view/telaInicio.java
@@ -1,6 +1,14 @@
 package view;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 public class telaInicio {
+
+    @FXML
+    private Button redirectPrimeiraTela;
+
+    @FXML
+    private Button redirectTutorial;
 
 }
