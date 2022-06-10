85d466ab5bd5343944f63a80e192a7198aad9022
Leonardo / LuKi <leo.monteiro06@live.com>
2022-06-10 17:42:22 -0300

    Reverter algumas alterações para consertar conflito

--- POO/Avaliação/src/view/FazerRegistrosController.java
+++ POO/Avaliação/src/view/FazerRegistrosController.java
@@ -1,14 +1,14 @@
 package view;
 
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.DatePicker;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Hyperlink;
-import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
-import model.Emprestimo; 
+import javafx.scene.layout.AnchorPane;
 
 public class FazerRegistrosController {
 
@@ -19,35 +19,35 @@ public class FazerRegistrosController {
     private Button btnEnviar;
 
     @FXML
-    private DatePicker diaEntrega;
+    private ComboBox<?> diaEntrega;
 
     @FXML
-    private TextField equipamento;
+    private ComboBox<?> equipamento;
 
     @FXML
-    private Hyperlink fazerRegistro;
-
-    @FXML
-    private TextField horarioEntrega;
+    private ComboBox<?> horarioEntrega;
 
     @FXML
     private ImageView iconConfig;
 
     @FXML
-    private ChoiceBox<?> nomeProfessor;
+    private AnchorPane labelText;
+
+    @FXML
+    private ComboBox<?> nomeProfessor;
 
     @FXML
     private Hyperlink verRegistros;
 
     @FXML
-    void abrirTelaInicio(ActionEvent event) {
+    void abrirTelaInicio(ActionEvent event) throws IOException {
         Funcoes funcoes = new Funcoes();
         btnEnviar.getScene().getWindow().hide();
         funcoes.mudarTela(event, "menuInicial.fxml", "Menu inicial");
     }
 
     @FXML
-    void abrirVerRegistros(ActionEvent event) {
+    void abrirVerRegistros(ActionEvent event) throws IOException {
         Funcoes funcoes = new Funcoes();
         btnEnviar.getScene().getWindow().hide();
         funcoes.mudarTela(event, "verRegistros.fxml", "Ver registros");
@@ -55,7 +55,7 @@ public class FazerRegistrosController {
 
     @FXML
     void enviar(ActionEvent event) {
-
+        
     }
-
+    
 }
