2973380b07ba32747eb8fd0f6b2f6bbac3b70c17
mark duarts <106616718+Marcsdurt@users.noreply.github.com>
2022-06-13 08:40:29 -0300

    adicionando protótipo de tutorial

---
+++ PW/view/telaTutorial.php
@@ -0,0 +1,73 @@
+<head>
+    <link
+    rel="stylesheet"
+    href="https://unpkg.com/swiper@8/swiper-bundle.min.css"
+    />
+
+    <script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
+    <style>
+        .swiper {
+            width: 304px;
+            height: 100%;
+            border: 1px solid black;
+            padding: 0 58px;
+        }
+
+        .swiper-wrapper {
+            margin: auto;
+            position: relative;
+            width: 100%;
+            height: 100%;
+            z-index: 1;
+            display: flex;
+            transition-property: transform;
+            box-sizing: content-box;
+            align-items: center;
+        }
+
+        .swiper-slide {
+            border: 1px solid black;
+            text-align: center;
+            font-size: 18px;
+            background: #fff;
+            display: -webkit-box;
+            display: -ms-flexbox;
+            display: -webkit-flex;
+            display: flex;
+            -webkit-box-pack: center;
+            -ms-flex-pack: center;
+            -webkit-justify-content: center;
+            justify-content: center;
+            -webkit-box-align: center;
+            -ms-flex-align: center;
+            -webkit-align-items: center;
+            align-items: center;
+            height: 80%;
+        }
+
+        .swiper-slide img {
+            display: block;
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+        }
+    </style>
+</head>
+<body>
+<div class="swiper mySwiper">
+      <div class="swiper-wrapper">
+        <div class="swiper-slide">Clique em "Ver registros", para visualizar a tela de materiais alugados.</div>
+        <div class="swiper-slide">Clique em "Ver registros", para visualizar a tela de materiais alugados.</div>
+        <div class="swiper-slide">Clique em "Ver registros", para visualizar a tela de materiais alugados.</div>
+      </div>
+      <div class="swiper-button-next"></div>
+      <div class="swiper-button-prev"></div>
+      <div class="swiper-pagination"></div>
+    </div>
+
+    <!-- Swiper JS -->
+    <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+
+    <script src="scripts/jquery.js"></script>
+    <script src="scripts/app.js"></script>
+</body>
