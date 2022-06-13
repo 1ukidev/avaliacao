03ca438fa79dcdc40ff9f630ec0489728fd59576
mark duarts <106616718+Marcsdurt@users.noreply.github.com>
2022-06-13 08:42:53 -0300

    linkando arquivo do swiper js

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>prova - pw</title>
+    <link rel="stylesheet" href="style/alertify.min.css">
+    <link rel="stylesheet" href="style/style.css">
+</head>
+<body>
+    <?php
+    /*
+        $dados = [];
+        $arr;
+    */
+    ?>
+    <script>
+        let string;
+        let dados = [];
+        let arr;
+    </script>
+    <div class="container">
+    <div class="coisas">
+            <img src="imagens/icon 1.png" alt="">
+            <span class="text1">Bem-Vindo(a)</span> 
+            <span class="text2">Deseja ver um breve tutorial</span>
+            <div class="btns">   
+              <button id="btn1">Claro!</button><br><br>
+              <button id="btn2">Não, obrigado(a)!</button>  
+            </div>    
+        </div>
+    </div>
+    <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+    <script src="scripts/jquery.js"></script>
+    <script src="scripts/app.js"></script>
+</body>
+</html>
