b1b96da3e3171b83a4fb02ee1e22c0261f901f66
Leonardo / LuKi <leo.monteiro06@live.com>
2022-06-13 22:08:34 -0300

    Pequena correção

--- POO/Avaliação/src/view/FazerRegistrosController.java
+++ POO/Avaliação/src/view/FazerRegistrosController.java
@@ -14,6 +14,7 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import model.*;
 import control.*;
+
 public class FazerRegistrosController implements Initializable {
     EmprestimoC emprestimoC = new EmprestimoC();
 
