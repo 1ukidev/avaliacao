077f6a3e83e0f766f5a3632b2aee0e8ef6f0a617
L0rdSaiyan <89421533+L0rdSaiyan@users.noreply.github.com>
2022-06-16 10:43:30 -0300

    Update TelaTutorialController.java

--- POO/Avaliação/src/view/TelaTutorialController.java
+++ POO/Avaliação/src/view/TelaTutorialController.java
@@ -4,7 +4,13 @@ import java.io.IOException;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import javafx.scene.control.Hyperlink;
+import javafx.scene.input.MouseEvent;
 
 public class TelaTutorialController {
 
@@ -14,17 +20,33 @@ public class TelaTutorialController {
     @FXML
     private Hyperlink verRegistros;
 
+    private Stage stage;
+    private Parent root;
+    private Scene scene;
+
     @FXML
-    void abrirTelaInicio(ActionEvent event) throws IOException {
-        Funcoes funcoes = new Funcoes();
-        Inicio.getScene().getWindow().hide();
-        funcoes.mudarTela(event, "menuInicial.fxml", "Menu inicial");
+    void abrirTelaInicio(MouseEvent event) throws IOException {
+
+        root = FXMLLoader.load(getClass().getResource("telainicio.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Home - Inicio");
+        stage.show();
+
     }
 
     @FXML
     void abrirVerRegistros(ActionEvent event) throws IOException {
-        Funcoes funcoes = new Funcoes();
-        verRegistros.getScene().getWindow().hide();
-        funcoes.mudarTela(event, "verRegistros.fxml", "Ver registros");
+
+        root = FXMLLoader.load(getClass().getResource("VerRegistros.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Registros");
+        stage.show();
+
+
     }
+
 }
