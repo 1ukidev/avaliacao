60764490e72623934e25d5f7b10d8ac0da0e3ed8
L0rdSaiyan <89421533+L0rdSaiyan@users.noreply.github.com>
2022-06-16 11:27:32 -0300

    Update Funcoes.java

--- POO/Avaliação/src/view/Funcoes.java
+++ POO/Avaliação/src/view/Funcoes.java
@@ -6,16 +6,21 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Funcoes {
     public void mudarTela(ActionEvent event, String path, String nomeDaJanela) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(path));
         Parent root = (Parent) fxmlLoader.load();
+        Image icon = new Image("imagens/icon.png");
         Stage stage = new Stage();
         stage.setResizable(false);
         stage.setTitle(nomeDaJanela);
         stage.setScene(new Scene(root));
+        stage.getIcons().add(icon);
         stage.show();
     }
+
+   
 }
